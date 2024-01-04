dbf895c9462ef26b49617e68deb7e0f1113b2b1e
inzamambutt <126397146+inzamambutt@users.noreply.github.com>
2024-01-04 16:31:55 +0000

    Add files via upload

---
+++ CovidPortfolioProject.sql
@@ -0,0 +1,126 @@
+/*
+Covid 19 Data Exploration 
+
+Skills used: Joins, CTE's, Temp Tables, Windows Functions, Aggregate Functions, Creating Views, Converting Data Types
+
+*/
+
+select * from PortfolioProject..CovidDeaths
+where continent is not null
+order by 3,4
+
+
+-- Intial Data
+
+select location, date, total_cases, new_cases, total_deaths,population
+from PortfolioProject..CovidDeaths
+where continent is not null
+order by 1,2
+
+
+-- Total Cases VS Total Deaths
+
+select location, date, total_cases, total_deaths, (cast(total_deaths as float)/cast(total_cases as float))*100 as DeathPercentage
+from PortfolioProject..CovidDeaths
+where continent is not null
+order by 1,2
+
+
+--Chances of dying if contracted Covid in UK
+
+select location, date, total_cases, total_deaths, (cast(total_deaths as float)/cast(total_cases as float))*100 as DeathPercentage
+from PortfolioProject..CovidDeaths
+where location like '%Kingdom%'
+order by 1,2
+
+
+-- Total Cases VS Population
+
+select location, date,population, total_cases, (cast(total_cases as decimal)/cast(population as decimal))*100 as InfectionRate
+from PortfolioProject..CovidDeaths
+order by 1,2
+
+
+-- Countries with highest infection rate
+
+select location, population, MAX(cast(total_cases as int)) as HighestInfectedCount, (cast(MAX(total_cases) as int)/cast(population as decimal))*100 as InfectionRate
+from PortfolioProject..CovidDeaths
+where continent is not null
+group by location, population
+order by InfectionRate desc
+
+
+--Countries with highest death rate
+
+select location, MAX(cast(total_deaths as int)) as TotalDeathCount
+from PortfolioProject..CovidDeaths
+where continent is not null
+group by location
+order by TotalDeathCount desc
+
+
+-- Contintents with the highest death count
+
+Select continent, MAX(cast(Total_deaths as int)) as TotalDeathCount
+From PortfolioProject..CovidDeaths
+Where continent is not null 
+Group by continent
+order by TotalDeathCount desc
+
+
+-- Number of deaths in the world
+
+Select SUM(new_cases) as TotalCases, SUM(cast(new_deaths as int)) as TotalDeaths, SUM(cast(new_deaths as int))/SUM(New_Cases)*100 as DeathPercentage
+From PortfolioProject..CovidDeaths
+where continent is not null 
+order by 1,2
+
+
+-- join tables
+
+select *
+from PortfolioProject.. CovidDeaths dea
+join PortfolioProject.. CovidVaccinations vac
+	on dea.location = vac.location
+	and dea.date = vac.date
+
+
+-- Number of people that got atleast 1 Vaccination
+
+Select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations,
+SUM(CONVERT(int,vac.new_vaccinations)) OVER (Partition by dea.Location Order by dea.location, dea.Date) as RollingPeopleVaccinated
+From PortfolioProject..CovidDeaths dea
+Join PortfolioProject..CovidVaccinations vac
+	On dea.location = vac.location
+	and dea.date = vac.date
+where dea.continent is not null 
+order by 2,3
+
+
+-- using CTE to perform calculation on partion from previous query
+
+With PopvsVac (Continent, Location, Date, Population, New_Vaccinations, RollingPeopleVaccinated)
+as
+(
+Select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
+, SUM(CONVERT(int,vac.new_vaccinations)) OVER (Partition by dea.Location Order by dea.location, dea.Date) as RollingPeopleVaccinated
+From PortfolioProject..CovidDeaths dea
+Join PortfolioProject..CovidVaccinations vac
+	On dea.location = vac.location
+	and dea.date = vac.date
+where dea.continent is not null 
+)
+Select *, (RollingPeopleVaccinated/Population)*100
+From PopvsVac
+
+
+-- creating view for later visualiation
+
+Create View PercentPopulationVaccinated as
+Select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
+, SUM(CONVERT(int,vac.new_vaccinations)) OVER (Partition by dea.Location Order by dea.location, dea.Date) as RollingPeopleVaccinated
+From PortfolioProject..CovidDeaths dea
+Join PortfolioProject..CovidVaccinations vac
+	On dea.location = vac.location
+	and dea.date = vac.date
+where dea.continent is not null 
